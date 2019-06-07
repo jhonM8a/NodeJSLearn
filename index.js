27d82dc31f0ba39a0ae9f6613d7de93c647788ee
jhonochoa <jhon.ochoa@unillanos.edu.co>
2019-06-06 19:47:37 -0500

    Consola de node y REPL

---
+++ index.js
@@ -0,0 +1,13 @@
+function a() {
+  console.log("Llamado a la funcion a");
+}
+function b() {
+  console.log("Llamado a la funcion b");
+}
+function c() {
+  console.log("Llamado a la funcion c");
+}
+
+a();
+b();
+c();
