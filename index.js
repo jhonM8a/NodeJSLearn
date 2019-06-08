57d3a4b0c6c15fd9e0d2b1c21ad5647d24f2a4bd
jhonochoa <jhon.ochoa@unillanos.edu.co>
2019-06-08 10:58:18 -0500

    Codigo con ejemplos de procesos: Sincronos y Asincronos

---
+++ index.js
@@ -0,0 +1,8 @@
+const add = require("./src/addition");
+const intensive = require("./src/intensive");
+//add.syncSum(1, 2);
+//add.asyncSum(5, 4);
+
+//intensive.simulateSync();
+
+intensive.simulateAsync();
